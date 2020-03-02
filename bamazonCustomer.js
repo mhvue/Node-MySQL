21b6e8b8839636c1e6235449432e0d6ed326d9b9
Mai Houa Vue <mhvue4@gmail.com>
2020-03-01 22:34:56 -0600

    added function to ask to buy or exit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    displayItems();
+    askUser();
   });
 
   function displayItems() {
@@ -28,7 +28,7 @@ connection.connect(function(err) {
             "\nProduct: " + res[i].product_name +
             "\nPrice: " + res[i].price); 
         }
-        askUser();
+        toBuy();
       });
   }
 
@@ -36,12 +36,30 @@ connection.connect(function(err) {
   //consider giving an option of EXIT 
 
 function askUser() {
-  //The first should ask them the ID of the product they would like to buy.
+    inquirer.prompt(
+    {
+        name:"firstQuest",
+        type: "list",
+        message: "Welcome! Would you like to buy an item or Exit?",
+        choices: ["Buy", "Exit"]
+    }).then(function(answer){
+        if(answer.firstQuest === "Exit") {
+            console.log("Good-Bye.");
+            connection.end();
+        }
+        else{
+            displayItems();
+    //The first should ask them the ID of the product they would like to buy.
+        }
+    })
+};
+
+function toBuy() {
     inquirer.prompt([
     {
         name: "buyWhat",
         type: "input",
-        message:"Which items would you like to purchase? Please use ID number. OR type EXIT to leave."
+        message:"Which items would you like to purchase? Please use ID number."
     },
     {//The second message should ask how many units of the product they would like to buy.
         name: "buyHowMany",
@@ -53,19 +71,14 @@ function askUser() {
             } else {
                 return false;
             }
-     },
+        },
     }]).then(function (answer) {
             chosenProd = answer.buyWhat;
 
-            if(chosenProd == "EXIT") {
-                console.log(chosenProd);
-                connection.end();
-            }
-            else{
             var query = "SELECT product_name FROM products WHERE item_id ="+ chosenProd;
             connection.query(query, function(err, res){
                 if (err) throw err;
-                console.log(res[0].product_name);
+                console.log("You picked: " + res[0].product_name);
             });
             chosenQuan= parseInt(answer.buyHowMany);
             var queryCheck= "SELECT stock_quantity FROM products WHERE item_id=" + chosenProd;
@@ -77,17 +90,15 @@ function askUser() {
                 }
                 else if (chosenQuan <= res[0].stock_quantity) {
                     newStock= res[0].stock_quantity - chosenQuan;
-                     console.log(newStock);
+                    // console.log(newStock);
                     updateProd();
                 }
                 else{
-                    console.log("Sorry we do not have enough. Please pick something else")
+                    console.log("Sorry we do not have enough.")
                     startOver();
                 }
-            
-            });
-        }      
-         });
+                });
+        })     
     };
 
 //this means updating the SQL database to reflect the remaining quantity.
@@ -105,9 +116,8 @@ function updateProd() {
             if (err) throw err;
         }
     )
-
     totalCost();
-}
+};
 
 // Once the update goes through, show the customer the total cost of their purchase.
 function totalCost() {
@@ -118,7 +128,7 @@ function totalCost() {
         console.log("Your total is " + total + ".");
         startOver();
     })
-}
+};
 
 function startOver() {
     inquirer.prompt({
@@ -136,4 +146,4 @@ function startOver() {
             connection.end();
         }
     })
-}
+};
